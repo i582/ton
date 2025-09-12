952664c743972d6a69f44da8110c09dea8c0b003
i582 <51853996+i582@users.noreply.github.com>
2025-09-12 15:45:42 +0400

    allocate logges on the heap

--- emulator/emulator-emscripten.cpp
+++ emulator/emulator-emscripten.cpp
@@ -190,7 +190,8 @@ void destroy_emulator(void* em) {
 }
 
 const char *emulate_sbs(void *em, const char* libs, const char* account, const char* message, const char* params) {
-    StringLog* logger = new StringLog;
+    // we need to allocate logger on the heap since it outlive this functions unlike `emulate_with_emulator`
+    StringLog* logger = new StringLog();
 
     td::log_interface = logger;
     SET_VERBOSITY_LEVEL(verbosity_DEBUG);
@@ -306,7 +307,8 @@ const char *emulate(const char *config, const char* libs, int verbosity, const c
 }
 
 void *setup_sbs_get_method(const char *params, const char* stack, const char* config) {
-    StringLog* logger = new StringLog;
+    // we need to allocate logger on the heap since it outlive this functions unlike `run_get_method`
+    StringLog* logger = new StringLog();
 
     td::log_interface = logger;
     SET_VERBOSITY_LEVEL(verbosity_DEBUG);
