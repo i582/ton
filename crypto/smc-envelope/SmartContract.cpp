1babc387a29432751daaeb5d7b07e3dc35ea546e
i582 <51853996+i582@users.noreply.github.com>
2025-09-12 23:44:33 +0400

    remove old no inspection comment

--- crypto/smc-envelope/SmartContract.cpp
+++ crypto/smc-envelope/SmartContract.cpp
@@ -357,7 +357,6 @@ SmartContract::Answer SmartContract::run_get_method(Args args) const {
                        args.vm_log_verbosity_level, args.debug_enabled, args.config ? args.config.value() : nullptr);
 }
 
-// ReSharper disable once CppDFAConstantFunctionResult
 int SmartContract::run_get_method_debug(Args args, std::unique_ptr<vm::VmState>& vm, std::unique_ptr<Logger>& logger) {
   prepare_get_method_args(args);
   // For debug mode we need only to setup VM for stepping
